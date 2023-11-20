875316b6855f6ecc9307d4e188c68ae6578a4771
Nasser Ben <nasserbenyed@gmail.com>
2023-11-20 01:17:28 -0800

    inital setup

---
+++ app/profile/layout.js
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }) {
+  return (
+    <div>
+      <h1 className="text-red-500">Sample Profile Layout</h1>
+      {children}
+    </div>
+  );
+}
---
+++ app/profile/page.js
@@ -0,0 +1,7 @@
+export default function Sample() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+    </div>
+  );
+}
