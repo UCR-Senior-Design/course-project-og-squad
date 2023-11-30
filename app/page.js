ddb3726c2f84023946a1aa3651612fa727e4cd2b
Maliha Meer <malihameer@hotmail.com>
2023-11-29 23:32:41 -0800

    added layout

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Post from "@/components/post";
-import image from "../assets/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg"
-import image2 from "../assets/Miso-Ramen.jpeg"
+import image from "../assets/pizza.png"
+import image2 from "../assets/ramen.png"
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <Post post={{ name: "Pizza", time: "30 mins", calories: "1000 cals", description: "bell pepper", imageURL: image }} />
-      <Post post={{ name: "Miso Ramen", time: "20 mins", calories: "1200 cals", description: "Carrots", imageURL: image2 }} />
+      <Post post={{ name: "Pizza", time: "30 mins", calories: "1000 cals", description: "Pizza description", imageURL: image }} />
+      <Post post={{ name: "Miso Ramen", time: "20 mins", calories: "1200 cals", description: "Miso-Ramen description", imageURL: image2 }} />
       <Footer />
     </main>
   );
--- components/post.jsx
+++ components/post.jsx
@@ -1,9 +1,7 @@
-
 import React from "react";
 import Image from "next/image";
 import { FaFireFlameCurved } from "react-icons/fa6";
 import { IoIosTimer } from "react-icons/io";
-
 
 export default function Post({ post }) {
   const { name, time, calories, description, imageURL } = post;
@@ -13,29 +11,24 @@ export default function Post({ post }) {
       {/* Display the square image if imageURL is provided */}
       {imageURL && (
         <div className="mr-4">
-          <Image src={imageURL} alt="Post Image" width={200} height={200} />
+          <div className="bg-orange-200 rounded-md overflow-hidden flex flex-col items-start p-4 pl-12" style={{ width: "300px", height: "400px" }}>
+            <Image src={imageURL} alt="Post Image" width={200} height={200} />
+            <h1 className="font-sans text-lg text-gray-800 font-bold mt-2">{name}</h1>
+            <div className="flex items-center mt-2">
+              <p className="ml-4"><IoIosTimer /></p>
+              <p style={{ color: '#FFA500' }} className="ml-2 pl-10"><FaFireFlameCurved /></p>
+            </div>
+            <div className="flex mt-2">
+              <p className="font-sans text-xs text-gray-800 mr-4">{time}</p>
+              <p className="font-sans text-xs text-gray-800">{calories}</p>
+            </div>
+            <p className="font-sans text-xs text-gray-800 mt-2">{description}</p>
+          </div>
         </div>
       )}
-  
-      <div className="flex flex-col">
-        <div className="flex items-center">
-          <h1 className="font-sans text-lg text-gray-800 font-bold py-2">{name}</h1>
-        </div>
-  
-      <div className="flex">
-        <p className="ml-4"><IoIosTimer /></p>
-        <p style={{ color: '#FFA500' }} className="ml-12"><FaFireFlameCurved /></p>
-      </div>
-        <div className="flex">
-         
-          <p className="font-sans text-xs text-gray-800 mr-4 mt-2">{time}</p>
-          
-          <p className="font-sans text-xs text-gray-800 mr-4 mt-2">{calories}</p>
-          
-          <p className="font-sans text-xs text-gray-800 mr-4 mt-2">{description}</p>
-        </div>
-        
-      </div>
+
+      {/* Rest of your content outside the orange background */}
+      {/* ... */}
     </div>
   );
 }
