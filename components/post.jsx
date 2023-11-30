b302f6810e0113c30b3abcf1a540a75c1e17de3e
Karl <karldrummer12@gmail.com>
2023-11-29 17:42:49 -0800

    Hot fix for post.jsx

--- components/post.jsx
+++ components/post.jsx
@@ -1,5 +1,3 @@
-
-import { Love_Ya_Like_A_Sister } from "next/font/google";
 import React from "react";
 import Image from "next/image";
 
@@ -8,22 +6,26 @@ export default function Post({ post }) {
 
   return (
     <div className="flex-col pl-8 py-4">
-      {/* Display the square image if imageURL is provided */}
       {imageURL && (
         <div className="mr-4">
-          <Image src={imageURL} alt="Post Image" width={200} height={200} />
+          <Image src={imageURL} alt={`Image for ${name}`} width={200} height={200} />
         </div>
       )}
-  
+
       <div className="flex flex-col">
         <div className="flex items-center">
           <h1 className="font-sans text-lg text-gray-800 font-bold">{name}</h1>
         </div>
         <div className="flex">
-          <p className="font-sans text-xs text-gray-800 mr-4 mt-2">{time}</p>
-          <p className="font-sans text-xs text-gray-800 mr-4 mt-2">{calories}</p>
+          <p className="font-sans text-xs text-gray-800 mr-4 mt-2">
+            <time>{time}</time>
+          </p>
+          <p className="font-sans text-xs text-gray-800 mr-4 mt-2">
+            <strong>{calories}</strong>
+          </p>
           <p className="font-sans text-xs text-gray-800 mr-4 mt-2">{description}</p>
         </div>
+      </div>
     </div>
   );
 }
