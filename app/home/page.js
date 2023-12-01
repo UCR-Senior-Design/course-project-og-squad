b35628995d54cac20e75fd629221523bd849b97e
Nasser Ben <nasserbenyed@gmail.com>
2023-12-01 01:03:43 -0800

    setup  mongo db to ssr and create api directory &  env file

---
+++ app/home/page.js
@@ -0,0 +1,23 @@
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import Post from "@/components/post";
+
+async function getHomePageRecipes() {
+  const res = await fetch("http://localhost:3000/api/homeRecipes");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getHomePageRecipes();
+
+  return (
+    <main>
+      <Navbar />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Footer />
+    </main>
+  );
+}
