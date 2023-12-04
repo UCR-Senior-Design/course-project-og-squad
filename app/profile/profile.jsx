4516e5e946e5a1e753caa43f6813e5abac5087fb
JoyHaddad <joyhaddad@gmail.com>
2023-12-04 12:50:57 -0800

    fix alignment

--- app/profile/profile.jsx
+++ app/profile/profile.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import settingIcon from "./Setting_fill.svg"
+import settingIcon from "./Setting_fill.svg";
 
 export default function Profile({ profile }) {
   const { userName, posts, followers, following, bio, imageURL } = profile;
@@ -7,7 +7,7 @@ export default function Profile({ profile }) {
   return (
     <div className="flex">
       {/* Left side: Profile information */}
-      <div className="w-1/4 p-4">
+      <div className="w-1/4 p-10">
         {/* Profile picture */}
         {imageURL && (
           <div className="mb-4 flex flex-col items-center">
@@ -15,31 +15,33 @@ export default function Profile({ profile }) {
 
             {/* Username with setting icon */}
             <div className="flex items-center mb-4">
-              <p className="text-lg font-semibold mr-2">{userName}</p>
-              <Image src={settingIcon} alt="Settings Icon" className="w-5 h-5" />
+              <p className="text-lg font-bold mr-2">{userName}</p>
+              <Image src={settingIcon} alt="Settings Icon" className="w-6 h-6" />
             </div>
 
+            {/* Counts and Bio container */}
 
-            {/* Counts */}
-            <div className="mb-4 flex justify-between">
-              <div className="text-center">
-                <h2 className="text-sm font-semibold mb-1">{posts}</h2>
-                <p className="text-xs text-gray-500">Posts</p>
+              {/* Counts */}
+              <div className="mb-4 flex justify-between">
+                <div className="text-center">
+                  <h2 className="text-sm font-semibold mb-1">{posts}</h2>
+                  <p className="text-xs text-gray-500">Posts</p>
+                </div>
+                <div className="text-center mx-6">
+                  <h2 className="text-sm font-semibold mb-1">{followers}</h2>
+                  <p className="text-xs text-gray-500">Followers</p>
+                </div>
+                <div className="text-center">
+                  <h2 className="text-sm font-semibold mb-1">{following}</h2>
+                  <p className="text-xs text-gray-500">Following</p>
+                </div>
               </div>
-              <div className="text-center mx-4">
-                <h2 className="text-sm font-semibold mb-1">{followers}</h2>
-                <p className="text-xs text-gray-500">Followers</p>
-              </div>
-              <div className="text-center">
-                <h2 className="text-sm font-semibold mb-1">{following}</h2>
-                <p className="text-xs text-gray-500">Following</p>
-              </div>
-            </div>
 
-            {/* Account bio */}
-            <div>
-              <p className="text-sm text-black-500">{bio}</p>
-            </div>
+              {/* Account bio */}
+              <div>
+                <p className="text-sm text-black-500">{bio}</p>
+              </div>
+
           </div>
         )}
       </div>
