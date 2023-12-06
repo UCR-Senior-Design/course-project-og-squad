d6af9df130e0302f6c55981b0a17af2ce223ae6f
Nasser Ben <nasserbenyed@gmail.com>
2023-12-05 16:55:06 -0800

    added custom main colors to tailwind and update sign in button

--- components/signInButton.jsx
+++ components/signInButton.jsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function SignInButton() {
   return (
     <Link href="/signin">
-      <button>Sign In</button>
+      <button className="bg-custom-main-dark px-4 py-2 rounded-lg text-white hover:bg-opacity-70">
+        Sign In
+      </button>
     </Link>
   );
 }
