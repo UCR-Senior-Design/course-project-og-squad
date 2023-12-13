237697a3e3cfe65af424bcccc62e85339b882e48
jeralsonep <jeralsoneparedes@gmail.com>
2023-12-12 22:44:44 -0800

    Revert "cleanup"
    This reverts commit f35c038dda10fabc7ee6ff213bd78abe78935e37.

--- components/signUpBox.jsx
+++ components/signUpBox.jsx
@@ -16,36 +16,39 @@ export default function SignUpBox() {
               <div className="border-2 w-10 border-custom-main-dark inline-block mb-2"></div>
               <p className=" text-xs mb-2">Fill in your credentials below.</p>
               <div className="flex flex-col items-center">
-                    {/* Username */}
-                    <h1 className="flex w-64 text-xs">Username</h1>
-                    <div className="bg-gray-200 w-64 p-2 flex items-center m-2 rounded-2xl">
-                        <FaRegUser className="text-gray-400 m-2" />
-                        <input
-                            type="username"
-                            name="username"
-                            placeholder="Enter username"
-                            className="border-2 bg-gray-200 
-                                        outline-none text-sm flex-1"
-                        />
-                    </div>
-                    {/* END OF: Username */}
-                    {/* Password */}
-                    <h1 className="flex w-64 text-xs">Password</h1>
-                    <div className="bg-gray-200 w-64 p-2 flex items-center m-2 rounded-2xl">
-                    <RiLockPasswordLine className="text-gray-400 m-2" />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Enter password"
-                        className="border-2 bg-gray-200 
-                                    outline-none text-sm flex-1"
-                    />
-                    </div>
-                    {/* END OF: Password */}
-                    <a href="#" className="border-2 border-custom-main-dark text-custom-main-dark 
-                            rounded-full px-12 py-2 mt-5 inline-block font-semibold
-                            hover:bg-custom-main-dark hover:text-white"> Sign Up
-                    </a>
+                {/* Username */}
+                <h1 className="flex w-64 text-xs">Username</h1>
+                <div className="bg-gray-200 w-64 p-2 flex items-center m-2 rounded-2xl">
+                  <FaRegUser className="text-gray-400 m-2" />
+                  <input
+                    type="username"
+                    name="username"
+                    placeholder="Enter username"
+                    className="border-2 bg-gray-200 
+                                outline-none text-sm flex-1"
+                  />
+                </div>
+                {/* END OF: Username */}
+                {/* Password */}
+                <h1 className="flex w-64 text-xs">Password</h1>
+                <div className="bg-gray-200 w-64 p-2 flex items-center m-2 rounded-2xl">
+                  <RiLockPasswordLine className="text-gray-400 m-2" />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    className="border-2 bg-gray-200 
+                                outline-none text-sm flex-1"
+                  />
+                </div>
+                {/* END OF: Password */}
+                <a
+                  href="#"
+                  className="border-2 border-custom-main-dark text-custom-main-dark rounded-full px-12 py-2 mt-5 inline-block font-semibold
+                            hover:bg-custom-main-dark hover:text-white"
+                >
+                  Sign Up
+                  </a>
               </div>
             </div>
                 <Link className="text-xs mt-3 text-right" href={"/signin"}>
