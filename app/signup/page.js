59ce5668e4c89263ed1525cddc3ead1858dedd8b
jeralsonep <jeralsoneparedes@gmail.com>
2023-12-12 22:35:16 -0800

    initial signup box added with page routing

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,9 +1,9 @@
-import SignInBox from "@/components/signInBox";
+import SignUpBox from "@/components/signUpBox";
 
 export default function SignUp() {
   return (
     <div className="my-40">
-      <SignInBox />
+      <SignUpBox />
     </div>
   );
 }
