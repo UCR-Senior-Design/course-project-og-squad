357ed374734d4caa72a60281052df21efe605665
JoyHaddad <joyhaddad@gmail.com>
2024-01-29 17:51:41 -0800

    fix profile page with grid

--- app/profile/posts.jsx
+++ app/profile/posts.jsx
@@ -5,20 +5,20 @@ export default function Posts({ posts }) {
   const { title, likeCount, imageURL } = posts;
 
   return (
-    <div className="flex">
+    <div className="mx-auto max-w-[300px]">
       {/* Left side: Profile information */}
-      <div className="w-3/4 p-10 rounded-md">
+      <div >
         {/* Post Picture */}
         {imageURL && (
-          <div className="mb-4 flex flex-col items-center">
+          <div className="flex bg-orange-100 p-5 rounded-lg flex-col items-center">
             <Image src={imageURL} alt="My Post" className="mb-2" height={175} width={175}/>
 
             {/* Title with Likes */}
             <div className="flex items-center">
               <p className="text-md font-bold mr-2">{title}</p>
               <div className="flex justify-between">
-                <div className="text-center">
-                  <FaRegHeart color="custom-main-dark" size={20} />
+                <div>
+                  <FaRegHeart className="cursor-pointer" color="FF9103" size={20} />
                   <p className="text-sm font-medium text-custom-main-dark">{likeCount}</p>
                 </div>
               </div>
