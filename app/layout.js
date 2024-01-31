55223f30e20d372f9ef4b4dc0836b01dee49dcd2
jeralsonep <jeralsoneparedes@gmail.com>
2024-01-30 17:06:56 -0800

    Snapchef favicon added

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,18 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import "./globals.css";
 
-import { AuthProvider } from "./Providers";
+import { AuthProvider } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "SnapChef",
-  description: "Made with next js",
+  title: 'SnapChef',
+  description: 'Your Culinary Connection to the World!',
+  icons:{
+    icon:['/faviconPackage/favicon.ico?v=4'],
+    apple:['/faviconPackage/apple-touch-icon.png'],
+    chrome:['/faviconPackage/android-chrome-192x192.png'],
+  }
 };
 
 export default function RootLayout({ children }) {
