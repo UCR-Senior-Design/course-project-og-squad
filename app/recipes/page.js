cc73db301ab819e65ac342ca38cda9133d820fc3
Maliha Fatima <malihameer@hotmail.com>
2024-02-07 15:31:13 -0800

    added steps button

--- app/recipes/page.js
+++ app/recipes/page.js
@@ -1,3 +1,5 @@
+
+
 "use client"
 import React, { useState } from 'react';
 import Image from "next/image";
@@ -11,6 +13,8 @@ import { LuVegan } from "react-icons/lu";
 
 const Recipes = () => {
   const [checkedItems, setCheckedItems] = useState({});
+  const [showIngredients, setShowIngredients] = useState(false);
+  const [showSteps, setShowSteps] = useState(false);
 
   const handleCheckboxChange = (event) => {
     setCheckedItems({
@@ -48,59 +52,79 @@ const Recipes = () => {
         </div>
       </div>
       <div className="mt-4">
-      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800" type="button">Ingredients 
-      <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-      </svg>
-</button>
-        <h2 className="text-lg font-semibold">Ingredients</h2>
-        <ul className="list-none m-0 p-0">
-          <li className="flex items-center">
-            <input
-              type="checkbox"
-              id="ingredient1"
-              className="rounded-full accent-orange-300 w-4 h-4 mr-2"
-              checked={checkedItems.ingredient1 || false}
-              onChange={handleCheckboxChange}
-            />
-            <label
-              htmlFor="ingredient1"
-              className={checkedItems.ingredient1 ? "line-through" : ""}
-            >
-              1 cup Olive Oil
-            </label>
-          </li>
-          <li className="flex items-center">
-            <input
-              type="checkbox"
-              id="ingredient2"
-              className="rounded-full accent-orange-300 w-4 h-4 mr-2"
-              checked={checkedItems.ingredient2 || false}
-              onChange={handleCheckboxChange}
-            />
-            <label
-              htmlFor="ingredient2"
-              className={checkedItems.ingredient2 ? "line-through" : ""}
-            >
-              1 cup Zaatar
-            </label>
-          </li>
-          <li className="flex items-center">
-            <input
-              type="checkbox"
-              id="ingredient3"
-              className="rounded-full accent-orange-300 w-4 h-4 mr-2"
-              checked={checkedItems.ingredient3 || false}
-              onChange={handleCheckboxChange}
-            />
-            <label
-              htmlFor="ingredient3"
-              className={checkedItems.ingredient3 ? "line-through" : ""}
-            >
-              Pita bread
-            </label>
-          </li>
-        </ul>
+        <button
+          id="dropdownDefaultButton"
+          onClick={() => setShowIngredients(!showIngredients)}
+          className="text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-orange-500 dark:hover:bg-orange-600 dark:focus:ring-orange-700"
+          type="button"
+        >
+          Ingredients
+          <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+          </svg>
+        </button>
+        <button
+    onClick={() => setShowSteps(!showSteps)} // Add this line to toggle the visibility of the steps
+    className="text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-orange-500 dark:hover:bg-orange-600 dark:focus:ring-orange-700 ml-2" // Add ml-2 for left margin
+    type="button"
+  >
+    Steps
+    <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+    </svg>
+  </button>
+        {showIngredients && (
+          <div>
+            <h2 className="text-lg font-semibold mt-4">Ingredients</h2>
+            <ul className="list-none m-0 p-0">
+              <li className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="ingredient1"
+                  className="rounded-full accent-orange-300 w-4 h-4 mr-2"
+                  checked={checkedItems.ingredient1 || false}
+                  onChange={handleCheckboxChange}
+                />
+                <label
+                  htmlFor="ingredient1"
+                  className={checkedItems.ingredient1 ? "line-through" : ""}
+                >
+                  1 cup Olive Oil
+                </label>
+              </li>
+              <li className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="ingredient2"
+                  className="rounded-full accent-orange-300 w-4 h-4 mr-2"
+                  checked={checkedItems.ingredient2 || false}
+                  onChange={handleCheckboxChange}
+                />
+                <label
+                  htmlFor="ingredient2"
+                  className={checkedItems.ingredient2 ? "line-through" : ""}
+                >
+                  1 cup Zaatar
+                </label>
+              </li>
+              <li className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="ingredient3"
+                  className="rounded-full accent-orange-300 w-4 h-4 mr-2"
+                  checked={checkedItems.ingredient3 || false}
+                  onChange={handleCheckboxChange}
+                />
+                <label
+                  htmlFor="ingredient3"
+                  className={checkedItems.ingredient3 ? "line-through" : ""}
+                >
+                  Pita bread
+                </label>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
       <div className="mt-5">
         <h3 className="text-lg font-semibold">Steps</h3>
@@ -111,3 +135,4 @@ const Recipes = () => {
 };
 
 export default Recipes;
+
