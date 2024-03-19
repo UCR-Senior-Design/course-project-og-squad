0aad97e2f516b276a2464ae97203f9b648ef9aa6
jeralsonep <jeralsoneparedes@gmail.com>
2024-03-18 23:49:59 -0700

    quick-add comment count

--- components/commentSection.jsx
+++ components/commentSection.jsx
@@ -15,14 +15,19 @@ export default function CommentSection({ commentData }) {
 
   return (
     <div className="max-w-4xl mx-auto p-6 shadow-md rounded-md my-6 border-4 border-[#575A65]">
-      <h2 className="text-2xl font-semibold mb-4">Comments</h2>
       <div className="max-h-80 overflow-y-auto" style={{ maxHeight: "400px" }}>
         {comments.length === 0 ? (
-          <p className="text-gray-500">No comments yet.</p>
+          <>
+            <h2 className="text-2xl font-semibold mb-4">Comments</h2>
+            <p className="text-gray-500">No comments yet.</p>
+          </>
         ) : (
-          comments.map((comment, index) => (
-            <Comment key={index} comment={comment} />
-          ))
+          <>
+            <h2 className="text-2xl font-semibold mb-4">Comments ({comments.length})</h2>
+            {comments.map((comment, index) => (
+              <Comment key={index} comment={comment} />
+            ))}
+          </>
         )}
       </div>
     </div>
